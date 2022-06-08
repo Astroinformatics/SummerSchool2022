483b702b0d6d78647de740508840b6458a466316
Eric Ford <ebf11@psu.edu>
2022-06-07 23:52:07 -0400

    add flux to download cudann artifact

--- tools/install_cuda.jl
+++ tools/install_cuda.jl
@@ -2,12 +2,14 @@ import Pkg
 Pkg.activate(mktempdir())
 Pkg.add([
         Pkg.PackageSpec(name="CUDA", version="3"),
+        Pkg.PackageSpec(name="Flux", version="0.13"),
         Pkg.PackageSpec(name="BenchmarkTools", version="1"),
         Pkg.PackageSpec(name="Plots", version="1"),
     ])
 Pkg.instantiate()
 
-using CUDA
+using CUDA, Flux
+
 x_h = rand(128)
 x_d = cu(x_h)
-
+x_d2 = x_h |> gpu
